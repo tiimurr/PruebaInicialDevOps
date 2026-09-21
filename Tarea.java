86322eca9249bde4c0b6c146c4e16fb503b5d5a5
tiimurr <tasecov01@educantabria.es>
2026-09-21 19:08:07 +0200

    Base del programa
    Primera versión del programa con apenas contenido

---
+++ Tarea.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author daw2
+ */
+public class Tarea {
+    String nombre;
+    String descripcion;
+}
